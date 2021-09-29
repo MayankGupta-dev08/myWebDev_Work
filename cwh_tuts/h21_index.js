457b76b130ac04f18045d68096df9a779157e2ac
Mayank Gupta <ce.mayank8@gmail.com>
2021-09-29 19:27:13 +0530

    after completing JS Project1: magicNotes-webApp-v1 on 29/9/21 - after adding 2 features to proj

--- cwh_tuts/h21_index.js
+++ cwh_tuts/h21_index.js
@@ -517,7 +517,43 @@ console.log(myDate.getFullYear());
 console.log(myDate.getDay());
 console.log(myDate.getMinutes());
 console.log(myDate.getHours());
+
+let today = new Date();
+// console.log(today);
+// console.log(typeof today);
+let otherDate = new Date('8-4-2003 04:54:08');
+// otherDate = new Date('June 13 1976');
+// otherDate = new Date('09/16/1976');
+console.log(otherDate);
+let a;
+a = otherDate.getDay();
+a = otherDate.getDate();
+a = otherDate.getMinutes();
+// a = otherDate.getSeconds();
+// a = otherDate.getHours();
+a = otherDate.getTime();
+a = otherDate.getMilliseconds();
+a = otherDate.getMonth();
+console.log(a);
+otherDate.setDate(23);
+otherDate.setMonth(0);
+otherDate.setFullYear(1900);
+otherDate.setMinutes(2);
+otherDate.setHours(1);
+otherDate.setSeconds(3);
+console.log(otherDate);
  
+let currTime = new Date();
+console.log(currTime);
+currTime.toLocaleString();
+console.log(currTime.toLocaleString());
+console.log(currTime.toDateString());
+console.log(currTime.toLocaleTimeString());
+
+let tme = currTime.toDateString() + ", " + currTime.toLocaleTimeString();
+console.log();
+console.log(tme);
+
 // DOM Manipulation
 let elem = document.getElementById('click');
 // console.log(elem);
--- cwh_tuts/js proj1/js/app_v2.js
+++ cwh_tuts/js proj1/js/app_v2.js
@@ -1,18 +1,37 @@
 console.log("Welcome to Magic Notes App, this is app.js");
 showNotes();
 
+// constantly changing the time after every 1000msec or 1sec
+setInterval(upDateTime, 1000);
+function upDateTime() {
+    let timeNow = document.getElementById('timeNow');
+    timeNow.innerHTML = new Date();
+}
+
+
 // If user adds a note, we will add it to local storage
 let addBtn = document.getElementById('addBtn');
-addBtn.addEventListener('click', function() {
+addBtn.addEventListener('click', function () {
     let addTxt = document.getElementById("addTxt");
+    let addTtl = document.getElementById("addTtl");
+    let currTime = new Date();
+    let tme = currTime.toDateString() + ", " + currTime.toLocaleTimeString();
+
     let notes = localStorage.getItem("notes");  //returns current value stored in local storage with the given key, and null if key doesn't exist
     if (notes == null) {
         notesObj = [];
     } else {
         notesObj = JSON.parse(notes); //convert string into a json obj
     }
-    notesObj.push(addTxt.value);
+
+    let myObj = {
+        title: addTtl.value,
+        text: addTxt.value,
+        time: tme
+    };
+    notesObj.push(myObj);
     localStorage.setItem("notes", JSON.stringify(notesObj));    //json obj into string
+    addTtl.value = "";
     addTxt.value = "";
     // console.log(notesObj);
     showNotes();
@@ -29,30 +48,34 @@ function showNotes() {
     let html = "";
     notesObj.forEach(function (element, index) {
         html += `
-                 <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
-                     <div class="card-body">
-                         <h5 class="card-title">Note ${index+1}</h5>
-                         <p class="card-text">${element}</p>
-                         <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
-                     </div>
-                 </div>`;
+                <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h6 class="card-no">Note${index + 1} @ ${element.time}</h6><hr>
+                        <h5 class="card-title">${element.title}</h5>
+                        <p class="card-text">${element.text}</p>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                            <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-danger">Delete</button>
+                            <button type="button" class="btn btn-success">Important</button>
+                        </div>
+                   </div>
+                </div>`;
     });
     let notesElm = document.getElementById("notes");
-    if(notesObj.length != 0){
+    if (notesObj.length != 0) {
         notesElm.innerHTML = html;
-    }else{
+    } else {
         notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
     }
 }
 
 // Function to delete a note
-function deleteNote(index){
+function deleteNote(index) {
     //   console.log("I am deleting", index);
 
     let notes = localStorage.getItem("notes");
-    if(notes == null){
+    if (notes == null) {
         notesObj = [];
-    }else{
+    } else {
         notesObj = JSON.parse(notes);
     }
     notesObj.splice(index, 1);
@@ -62,18 +85,18 @@ function deleteNote(index){
 
 // function for searching from navbar in the webpage
 let search = document.getElementById('searchTxt');
-search.addEventListener('input', function(){
-    
+search.addEventListener('input', function () {
+
     let srchInput = search.value.toLowerCase();
     // console.log('Input event fired!', srchInput);
-    
+
     let noteCards = document.getElementsByClassName('noteCard');
-    Array.from(noteCards).forEach(function(element){
+    Array.from(noteCards).forEach(function (element) {
         let cardTxt = element.getElementsByTagName('p')[0].innerText;
         // console.log(cardTxt);
-        if(cardTxt.includes(srchInput)){
+        if (cardTxt.includes(srchInput)) {
             element.style.display = "block";
-        }else{
+        } else {
             element.style.display = "none";
         }
     });
@@ -81,8 +104,9 @@ search.addEventListener('input', function(){
 
 /*
 Further Features:
-1. Add Title
+1. Add Title -> Done
 2. Mark a note as Important
-3. Separate notes by user
-4. Sync and host to web server
+3. Adding time stamps to the notes -> Done
+4. Separate notes by user
+5. Sync and host to web server
 */
