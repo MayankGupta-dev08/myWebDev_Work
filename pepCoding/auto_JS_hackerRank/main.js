2cfdf65752397dd1649cef6324afd6e7302b4756
Mayank Gupta <ce.mayank8@gmail.com>
2021-10-24 12:48:42 +0530

    completed the project

--- pepCoding/auto_JS_hackerRank/main.js
+++ pepCoding/auto_JS_hackerRank/main.js
@@ -5,9 +5,9 @@
 // npm install minimist
 // npm install puppeteer
 
-const fs = require('fs');
-const minimist = require('minimist');
-const puppeteer = require('puppeteer');
+const fs = require("fs");
+const minimist = require("minimist");
+const puppeteer = require("puppeteer");
 
 const args = minimist(process.argv);
 
@@ -19,7 +19,7 @@ async function run() {
         headless: false,
         defaultViewport: null,
         slowMo: 20,
-        args: ['--start-maximized']
+        args: ["--start-maximized"],
     });
 
     let tabs = await browser.pages();
@@ -28,51 +28,69 @@ async function run() {
     await page.goto(args.url);
 
     // go to the home page of hackerRank
-    await page.waitForSelector('a[href="https://www.hackerrank.com/auth/signup"]', { visible: true });
+    await page.waitForSelector(
+        'a[href="https://www.hackerrank.com/auth/signup"]',
+        { visible: true }
+    );
     await page.click('a[href="https://www.hackerrank.com/auth/signup"]');
 
-    // click on the login button
+    // click on the login section
     await page.waitForSelector('a[href="/auth/login"]', { visible: true });
     await page.click('a[href="/auth/login"]');
 
     // enter the login details
     await page.waitForSelector('input[name="username"]', { visible: true });
     await page.type('input[name="username"]', configJSO.userId, { delay: 10 });
-    await page.type('input[name="password"]', configJSO.password, { delay: 10 });
+    await page.type('input[name="password"]', configJSO.password, {
+        delay: 10,
+    });
 
     // click on the login button
-    await page.waitForSelector('button[data-analytics="LoginPassword"]', { visible: true });
+    await page.waitForSelector('button[data-analytics="LoginPassword"]', {
+        visible: true,
+    });
     await page.click('button[data-analytics="LoginPassword"]');
 
     // go the the compete page of hackerRank
-    await page.waitForSelector('a[data-analytics="NavBarContests"]', { visible: true });
+    await page.waitForSelector('a[data-analytics="NavBarContests"]', {
+        visible: true,
+    });
     await page.click('a[data-analytics="NavBarContests"]');
 
     // go the the compete page of hackerRank
-    await page.waitForSelector('a[href="/administration/contests/"]', { visible: true });
+    await page.waitForSelector('a[href="/administration/contests/"]', {
+        visible: true,
+    });
     await page.click('a[href="/administration/contests/"]');
 
+    // getting the total contest pages (10 contest per page)
     await page.waitForSelector('a[data-attr1="Last"]', { visible: true });
-    let cPageNum = await page.$eval('a[data-attr1="Last"]', function (anchorTag) {
-        let cpages = parseInt(anchorTag.getAttribute('data-page'));
-        return cpages;
-    });
+    let cPageNum = await page.$eval(
+        'a[data-attr1="Last"]',
+        function (anchorTag) {
+            let cpages = parseInt(anchorTag.getAttribute("data-page"));
+            return cpages;
+        }
+    );
 
     for (let i = 1; i <= cPageNum; i++) {
         await handlePage(page, browser);
 
         if (i != cPageNum) {
-            await page.waitForSelector('a[data-attr1="Right"]', { visible: true });
+            await page.waitForSelector('a[data-attr1="Right"]', {
+                visible: true,
+            });
             await page.click('a[data-attr1="Right"]');
         }
     }
 
+    console.log("Done adding all the moderators to all the contests.");
 }
 
-// getting all the contest urls of page1
 async function handlePage(page, browser) {
-    await page.waitForSelector('a.backbone.block-center', { visible: true });
-    let curls = await page.$$eval('a.backbone.block-center', function (aTags) {
+    // getting all the contest urls of a given contest page
+    await page.waitForSelector("a.backbone.block-center", { visible: true });
+    let curls = await page.$$eval("a.backbone.block-center", function (aTags) {
         let links = [];
         for (let i = 0; i < aTags.length; i++) {
             let link = aTags[i].getAttribute("href");
@@ -81,28 +99,37 @@ async function handlePage(page, browser) {
         return links;
     });
 
-    for (let i = 0; i < curls.length; i++) {
+    // handling all the contest of a given page
+    await handleContest(browser, curls);
+    await page.waitForTimeout(1000);
+}
+
+async function handleContest(browser, cLinks) {
+    // adding moderators to each of the contest of the same page
+    for (let i = 0; i < cLinks.length; i++) {
         let ctab = await browser.newPage();
-        for (let i = 0; i < configJSO.moderators.length; i++) {
-            const element = configJSO.moderators[i];
-            await addModerator(ctab, args.url + curls[i], element);
+        await ctab.goto(args.url + cLinks[i]);
+        await ctab.bringToFront();
+
+        // clicking on the moderators section
+        await ctab.waitForSelector('li[data-tab="moderators"]', { visible: true });
+        await ctab.click('li[data-tab="moderators"]');
+        await ctab.waitForTimeout(2000);
+
+        // adding all the moderators to a contest
+        for (let j = 0; j < configJSO.moderators.length; j++) {
+            await addModerator(ctab, configJSO.moderators[j]);
         }
+
+        // closing the contest tab after all the moderators are added
         await ctab.close();
-        await page.waitForTimeout(1000);
     }
 }
 
-async function addModerator(ctab, fullUrl, moderatorName) {
-    await ctab.goto(fullUrl);
-    await ctab.bringToFront();
-
-    await ctab.waitForSelector('li[data-tab="moderators"]', { visible: true });
-    await ctab.click('li[data-tab="moderators"]');
-    await ctab.waitForTimeout(2000);
-
-    await ctab.waitForSelector('input#moderator', { visible: true });
-    await ctab.type('input#moderator', moderatorName, { delay: 30 });
-    await ctab.keyboard.press('Enter');
+async function addModerator(ctab, moderatorName) {
+    await ctab.waitForSelector("input#moderator", { visible: true });
+    await ctab.type("input#moderator", moderatorName, { delay: 30 });
+    await ctab.keyboard.press("Enter");
 
     await ctab.waitForTimeout(1000);
 }
