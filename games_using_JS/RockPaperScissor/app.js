3748b6becd9383e67e0039012b3d41019f1ae27b
Mayank Gupta <ce.mayank8@gmail.com>
2022-03-07 22:29:22 +0530

    games using js project started

---
+++ games_using_JS/RockPaperScissor/app.js
@@ -0,0 +1,10 @@
+const compChoiceDisplay = document.getElementById('compChoice');
+const userChoiceDisplay = document.getElementById('userChoice');
+const resultDisplay = document.getElementById('result');
+
+const possibleChoices = document.querySelectorAll('rpsBtn');
+
+let userChoice;
+let compChoice;
+
+possibleChoices.forEach()
