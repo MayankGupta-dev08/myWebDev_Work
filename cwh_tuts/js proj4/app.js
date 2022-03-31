44a59dd1eae3b8140bff14d787f8000b54964b85
Mayank Gupta <ce.mayank8@gmail.com>
2022-03-31 18:23:55 +0530

    completed form validation project using vanilla js

--- cwh_tuts/js proj4/app.js
+++ cwh_tuts/js proj4/app.js
@@ -1,46 +1,52 @@
-// console.log("file is included");
+console.log("form validation js file included");
 
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const phoneNumber = document.getElementById("phoneNumber");
+const submit = document.getElementById("submit");
+const success = document.getElementById("success");
+const failure = document.getElementById("failure");
+
+success.style.display = "none";
+failure.style.display = "none";
 
 let validEmail = false;
 let validUser = false;
 let validPhone = false;
 
-// using jQuery
-// $('#success').hide();
-// $('#failure').hide();
-
 username.addEventListener('blur', function () {
   console.log("usename is blurred");
-  let regex = /^[a-zA-Z]([0-9a-zA-Z]){3,10}$/;
+  let regex = /^[a-zA-Z]([0-9a-zA-Z]){4,10}$/;
   let str = username.value;
   console.log(regex, str);
   if (regex.test(str)) {
     console.log("Your username is valid");
     username.classList.remove("is-invalid");
+    username.classList.add("is-valid");
     validUser = true;
   } else {
     console.log("Your username is invalid");
+    username.classList.remove("is-valid");
     username.classList.add("is-invalid");
-    validUser = true;
+    validUser = false;
   }
 });
 
 email.addEventListener('blur', function () {
   console.log("email is blurred");
-  let regex = /^[a-zA-Z]([0-9a-zA-Z]){3,10}$/;
+  let regex = /^([0-9a-zA-Z_\.]+)@([\.0-9a-zA-Z]+)\.([a-zA-Z]){2,5}$/;
   let str = email.value;
   console.log(regex, str);
   if (regex.test(str)) {
     console.log("Your email is valid");
     email.classList.remove("is-invalid");
+    email.classList.add("is-valid");
     validEmail = true;
   } else {
     console.log("Your email is invalid");
     email.classList.add("is-invalid");
-    validEmail = true;
+    email.classList.remove("is-valid");
+    validEmail = false;
   }
 });
 
@@ -52,10 +58,26 @@ phoneNumber.addEventListener('blur', function () {
   if (regex.test(str)) {
     console.log("Your phoneNumber is valid");
     phoneNumber.classList.remove("is-invalid");
+    phoneNumber.classList.add("is-valid");
     validPhone = true;
   } else {
     console.log("Your phoneNumber is invalid");
     phoneNumber.classList.add("is-invalid");
-    validPhone = true;
+    phoneNumber.classList.remove("is-valid");
+    validPhone = false;
   }
 });
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log("submit button is clicked");
+
+  if (validUser && validPhone && validEmail) {
+    console.log("All the 3 things a valid. Submitting the form!");
+    success.style.removeProperty("display");
+  } else {
+    console.error("Unable to submit the form");
+    console.log("Please enter valid values");
+    failure.style.removeProperty("display");
+  }
+})
