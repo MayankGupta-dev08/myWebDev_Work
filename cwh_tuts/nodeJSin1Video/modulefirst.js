7ab96e22ecf83965cdadbbbbbbcdb11ff9c96b8f
Mayank Gupta <ce.mayank8@gmail.com>
2022-04-06 13:44:17 +0530

    nodeJS in one video

---
+++ cwh_tuts/nodeJSin1Video/modulefirst.js
@@ -0,0 +1,7 @@
+// import {simple, simple2} from "./modulesecond.mjs"
+import * as a2 from "./modulesecond.mjs"
+// const simple2 = require("./modulesecond.mjs");
+// const simple2 = require("./modulesecond.js");
+
+// simple23()
+console.log(a2.simple())
